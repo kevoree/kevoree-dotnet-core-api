fa706c6e3c3126f1d80c0674353536d271a7c78b
Manuel Leduc <manuel.leduc@inria.fr>
2015-08-28 17:46:58 +0200

    Version before real classloading tests

---
+++ BootstrapService.cs
@@ -0,0 +1,15 @@
+﻿using org.kevoree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Org.Kevoree.Core.Api
+{
+    public interface BootstrapService
+    {
+        void injectDictionary(Instance instance, IRunner target, bool onlyDefault);
+
+        IRunner createInstance(ContainerNode nodeInstance);
+    }
+}
---
+++ IRunner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Kevoree.Core.Api
+{
+    public interface IRunner
+    {
+    }
+}
